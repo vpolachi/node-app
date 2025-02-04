f748487aef2ead480f2e56a5ae61f4b2eb8f1760
Harish Reddy <113511497+harish981@users.noreply.github.com>
2025-02-04 12:54:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Kubernetes with Kustomize!\n');
+    res.end('Hi, Kubernetes with Kustomize!\n');
 });
 
 server.listen(PORT, () => {
